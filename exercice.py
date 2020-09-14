c38be6cb979ba0e4a2b6a18c26e9ae7a0c0bc77e
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-14 22:43:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return temperature
+    return (temperature*9/5)+
 
 
 def to_farenheit(temperature: float) -> float:
