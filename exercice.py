2470939835165f412396b4c685fabda4557c902a
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-14 23:01:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return (angle_degs + (angle_mins/60)/60)*(math.pi)
+    return (angle_degs + (angle_mins/60)/60)*(math.pi/18)
 
 
 def to_degrees(angle_rads: float) -> tuple:
